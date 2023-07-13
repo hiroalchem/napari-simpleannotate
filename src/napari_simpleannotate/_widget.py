622256f51d2b109e8ff92be08dfc2ad8e6b3ffbf
hiroalchem <h.kawai888@gmail.com>
2023-07-13 10:56:36 +0900

    fist commit

---
+++ src/napari_simpleannotate/_widget.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+
+if TYPE_CHECKING:
+    import napari
+
+
+class BboxQWidget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+
+        btn = QPushButton("Click me!")
+        btn.clicked.connect(self._on_click)
+
+        self.setLayout(QHBoxLayout())
+        self.layout().addWidget(btn)
+
+    def _on_click(self):
+        print("napari has", len(self.viewer.layers), "layers")
